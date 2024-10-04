bf70d10bbb3e45b76aa24c6255ffc84aa3af3569
“Vitalemonteea” <2192189495@qq.com>
2024-10-04 22:53:52 +0800

    first commit dj --error

--- Dijkstra.m
+++ Dijkstra.m
@@ -1,14 +1,32 @@
 %Dijkstra算法实现
-
 % 初始化
 % 输入邻接矩阵，开始的点以及终点，返还最短路径
-funtion [ min , path] = dijkstra (G , start_point ,end_point) 
-    num_nodes = size(G);   %记录nodes个数
-    min = inf (num_nodes);  
+function[ distances , path] = Dijkstra (G, start_point) 
+    num_nodes = size(G,1);   %记录nodes个数
+    distances = inf (num_nodes);  
     path = zeros(num_nodes);  
     visited = false(num_nodes);   % 记录是否已经遍历
     
 
-    min(start_point) = 0; 
+    distances(start_point) = 0;
+
+
+   for i=1:num_nodes
+       %寻找未访问的最小距离点
+       [~, current_point]= min(distances(~visited));   %把distances的最小点记录
+       visited(current_point) = true;
+       
+       %更新distances矩阵
+       for neighbor = 1: num_nodes
+           if ~visited(neighbor)&& G(current_point,neighbor)>0
+               new_distance = distances(current_point) + G(current_point,neighbor);
+               if new_distance < distances(neighbor)
+                   distances(neighbor) = new_distance;
+                   path(neighbor)= current_point; % 设置前驱节点
+               end
+           end
+       end          
+   end
+end
 
     
