1441b4baf9ebbc172fb0fbe9a3ce04ec2dbddebb
“Vitalemonteea” <2192189495@qq.com>
2024-10-11 16:37:42 +0800

    优化

--- assignment1.m
+++ assignment1.m
@@ -25,12 +25,75 @@ remaining_points = all_points([keep_points, remaining_points_index], :);
 % 优化只查询自己最近的9个点位置
 num_remaining_points = size(remaining_points, 1);
 G = zeros(num_remaining_points);
+for i = 1:num_remaining_points
+    for j = 1:num_remaining_points
+        % 计算两点之间的距离
+        dist = norm(remaining_points(i,:) - remaining_points(j,:));
+        if abs(remaining_points(i,1) - remaining_points(j,1)) <= 1 &&...
+           abs(remaining_points(i,2) - remaining_points(j,2)) <= 1
+            G(i,j) = dist;
+            G(j,i) = dist; % 因为是无向图，所以邻接矩阵是对称的
+        end
+    end
+end
 
-% ... 后面的代码保持不变 ...
 
 % 在绘图之前，打印剩余点的数量
 disp(['Number of remaining points: ', num2str(num_remaining_points)]);
 
-% ... 绘图代码 ...
+disp(size(G))
+%调用Dijkstra算法
+start_point = 1; % (1,1) 的索引
+end_point = num_remaining_points; % (20,20) 的索引
+[distances, path] = Dijkstra(G, start_point);
+
+% 重构最短路径
+shortest_path = end_point;
+while shortest_path(1) ~= start_point
+    shortest_path = [path(shortest_path(1)), shortest_path];
+end
+
+% 绘制图的节点和边
+figure;
+hold on;
+num_remaining_points = size(remaining_points, 1);
+
+% 绘制边
+for i = 1:num_remaining_points
+    for j = i+1:num_remaining_points % 只遍历后面的点，避免重复绘制边
+        if G(i,j) > 0
+            plot([remaining_points(i,1), remaining_points(j,1)], [remaining_points(i,2), remaining_points(j,2)], 'k-');
+        end
+    end
+end
+
+% 绘制节点
+for i = 1:num_remaining_points
+    if ismember(i, [1, num_remaining_points]) % 起点和终点
+        plot(remaining_points(i,1), remaining_points(i,2), 'go', 'MarkerSize', 8, 'MarkerFaceColor', 'g');
+    else
+        plot(remaining_points(i,1), remaining_points(i,2), 'go', 'MarkerSize', 6, 'MarkerFaceColor', 'g');
+    end
+end
+
+% 绘制被移除的点
+removed_points = all_points(removed_points_index, :);
+plot(removed_points(:,1), removed_points(:,2), 'ro', 'MarkerSize', 4);
+
+% 绘制最短路径
+path_x = remaining_points(shortest_path, 1);
+path_y = remaining_points(shortest_path, 2);
+plot(path_x, path_y, 'b-', 'LineWidth', 2);
+
+axis([0 21 0 21]); 
+xticks(0:1:21);
+yticks(0:1:21);
+grid on;
+hold off;
+xlabel('X轴');
+ylabel('Y轴');
+title('邻接矩阵可视化与最短路径');
+legend('边', '保留的点', '移除的点', '最短路径', 'Location', 'best');
+
 
 
