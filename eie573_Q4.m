fc2c37e2f7a62e7bde8fc3713f1de5ec30c1c136
“Vitalemonteea” <2192189495@qq.com>
2024-10-24 19:22:48 +0800

    modify Q4

--- eie573_Q4.m
+++ eie573_Q4.m
@@ -11,8 +11,8 @@ F = 24.067358;
 Pt_dBm = 10 * log10(Pt * 1000);
 
 % Generate realizations
-num_realizations = int32(F * 1e7);  % F * 10^7 realizations
-psi_dB = normrnd(0, sigma_psi_dB, [1, num_realizations]);
+num_realizations = round(F * 1e7);  % Use round() instead of int32()
+psi_dB = sigma_psi_dB * randn(1, num_realizations);
 
 % Calculate path loss
 PL = -(10 * log10(K) + 10 * gamma * log10(d0/d));
@@ -20,7 +20,7 @@ PL = -(10 * log10(K) + 10 * gamma * log10(d0/d));
 % Calculate received power in dBm for each realization
 Pr_dBm = Pt_dBm - PL + psi_dB;
 
-% Calculate outage probability for different thresholds
+% Calculate outage probability 
 P_min_dBm = -90;
 outage_count = sum(Pr_dBm < P_min_dBm);
 outage_probability = outage_count / num_realizations;
